d117c6ad5f7910f32a166c5cbe1d98b72c80ed08
Yatseyko Yuriy <yatseykoyuriy@MacBook-Pro-Yatseyko.local>
2018-03-08 15:27:52 +0200

    Initial Commit

---
+++ ARKit test apps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKit test apps
+//
+//  Created by Yatseyko Yuriy on 08.03.2018.
+//  Copyright © 2018 Yatseyko Yuriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
