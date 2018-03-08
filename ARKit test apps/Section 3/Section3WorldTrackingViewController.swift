0c85e060b7d86bad31b57c2e0ec31cee9a4f2c26
Yatseyko Yuriy <yatseykoyuriy@MacBook-Pro-Yatseyko.local>
2018-03-08 15:31:48 +0200

    Initial commit.

---
+++ ARKit test apps/Section 3/Section3WorldTrackingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Section3WorldTrackingViewController.swift
+//  ARKit test apps
+//
+//  Created by Yatseyko Yuriy on 08.03.2018.
+//  Copyright © 2018 Yatseyko Yuriy. All rights reserved.
+//
+
+import UIKit
+
+class Section3WorldTrackingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
