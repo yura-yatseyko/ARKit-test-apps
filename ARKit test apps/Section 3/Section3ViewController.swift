86a6d2fdf28c03515b0682b3cdfbd0c4161b2caa
Yatseyko Yuriy <yatseykoyuriy@MacBook-Pro-Yatseyko.local>
2018-03-09 13:48:55 +0200

    Rotation complated.

--- ARKit test apps/Section 3/Section3ViewController.swift
+++ ARKit test apps/Section 3/Section3ViewController.swift
@@ -39,32 +39,66 @@ class Section3ViewController: UIViewController {
     func getRandomNumbers(firstNumber: CGFloat, secondNumber: CGFloat) -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNumber - secondNumber) + min(firstNumber, secondNumber)
     }
+    
+    func addHouse() {
+        let  doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let node = SCNNode()
+        //        let path = UIBezierPath()
+        //        path.move(to: CGPoint(x: 0, y: 0))
+        //        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        //        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+        //        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        //        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        //        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        //        node.geometry = shape
+        
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        node.position = SCNVector3(0.2, 0.3, -0.2)
+        node.eulerAngles = SCNVector3(Float(180.degreesToRadians), 0, 0)
+        
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0, -0.02, 0.053)
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+    }
+    
+    func rotation() {
+        let cylinderNode = SCNNode(geometry: SCNCylinder(radius: 0.1, height: 0.15))
+        cylinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        cylinderNode.position = SCNVector3(0, 0, -0.3)
+        cylinderNode.eulerAngles = SCNVector3(0, 0, Float(90.degreesToRadians))
+        
+        let pyramidNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+        pyramidNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        pyramidNode.position = SCNVector3(0, 0, -0.5)
+        
+        cylinderNode.addChildNode(pyramidNode)
+        
+        sceneView.scene.rootNode.addChildNode(cylinderNode)
+    }
 
     // MARK: - Actions
     @IBAction func addButtonPressed(_ sender: Any) {
-        let node = SCNNode()
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: 0, y: 0.2))
-        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
-        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
-        path.addLine(to: CGPoint(x: 0.4, y: 0))
-        let shape = SCNShape(path: path, extrusionDepth: 0.2)
-        node.geometry = shape
-//        node.geometry = SCNSphere(radius: 0.1)
-//        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.1, height: 0.3)
-//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
-//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-        node.geometry?.firstMaterial?.specular.contents = UIColor.white
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        let x = getRandomNumbers(firstNumber: -0.3, secondNumber: 0.3)
-        let y = getRandomNumbers(firstNumber: -0.3, secondNumber: 0.3)
-        let z = getRandomNumbers(firstNumber: -0.3, secondNumber: 0.3)
-        node.position = SCNVector3(0, 0, -0.3)
-        sceneView.scene.rootNode.addChildNode(node)
+        addHouse()
     }
     
     @IBAction func resetButtonPressed(_ sender: Any) {
         restartSession()
+    }
+}
+
+extension Int {
+    var degreesToRadians: Double {
+        return Double(self) * .pi / 180
     }
 }
